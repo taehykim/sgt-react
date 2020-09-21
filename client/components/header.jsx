8c3c7821f23e8f647b4b6b3ec3bcee0021477a62
Tia <tiarekim@gmail.com>
2020-09-21 22:51:29 +0000

    added a feature to view students' average grade

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <h1 className="mb-3">Student Grade Table</h1>;
+    return (
+      <div className="row header-group">
+        <h1 className="mb-3">Student Grade Table</h1>
+        <h2>
+          Average Grade
+          <span className="badge badge-secondary ml-2">
+            {this.props.average ? this.props.average : '?N/A'}
+          </span>
+        </h2>
+      </div>
+    );
   }
 }
 
